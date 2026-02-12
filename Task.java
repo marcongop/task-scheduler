963172459c30fe98a2fe9afbc2dba460bcede97b
Joseph marc NGOP NDJEPENDA <josephmarcngopndjependa@MacBook-Air-de-Joseph.local>
2026-02-12 17:00:42 +0100

    Ajout de Task, TaskManager et Main

--- Task.java
+++ Task.java
@@ -1,12 +1,26 @@
 public class Task {
-
-    private int id;
-    private int duration;
+    private int id; // id de la tâche 
+    private int duration ;// La durée de la tâches (en unités de temps)
     private int priority;
 
+// mon constructeur
     public Task(int id, int duration, int priority) {
-        this.id = id;
-        this.duration = duration;
-        this.priority = priority;
+        this.id= id;
+        this.duration= duration;
+        this.priority= priority;
     }
+
+// getters
+    public int getId(){
+        return id;
+    }
+    public int getDuration(){
+        return duration;
+    }
+    public int getPriority(){
+        return priority;
+    }
+// Ma classe permet de définir une tâche avec un identifiant, une durée et une priorité.
+
+
 }
