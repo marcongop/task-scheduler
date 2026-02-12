653690d622106513e810ae99321cacdae64299da
Joseph marc NGOP NDJEPENDA <josephmarcngopndjependa@MacBook-Air-de-Joseph.local>
2026-02-12 15:31:07 +0100

    Create Task class

---
+++ Task.java
@@ -0,0 +1,12 @@
+public class Task {
+
+    private int id;
+    private int duration;
+    private int priority;
+
+    public Task(int id, int duration, int priority) {
+        this.id = id;
+        this.duration = duration;
+        this.priority = priority;
+    }
+}
